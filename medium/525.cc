0d2a137b7ff5409c65d128bea3000a2fb47942ad
Lorian Richmond <58707942+b4shfire@users.noreply.github.com>
2022-02-04 00:30:48 +0000

    Create 525.cc

---
+++ medium/525.cc
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        
+        const int N = nums.size();
+        
+        // maps i => j, where i is the smallest number such that nums[0:i] contains j more 1s than 0s
+        unordered_map<int, int> firsts;
+        firsts[0] = -1;
+        
+        int longest = 0;
+        int balance = 0;
+        
+        for (int i=0; i<N; ++i){
+            
+            balance += (nums[i]==1 ? 1 : -1);
+            
+            auto prev = firsts.find(balance);
+            
+            if (prev == firsts.end()) firsts[balance] = i;
+            else longest = max(longest, i - prev->second);
+        }
+
+        return longest;        
+    }
+};
