b22a33982dec4661da02b74b5b6eceb495f926bc
Lorian Richmond <58707942+b4shfire@users.noreply.github.com>
2022-03-19 09:53:02 +0000

    Create 895.cc

---
+++ hard/895.cc
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/maximum-frequency-stack/
+
+class FreqStack {
+    
+    // needed for selecting the element nearest to the top of the stack
+    long _num_pushes = 0;
+    
+    // maps val => number of occurences
+    map<int, int> _counts;
+    
+    // maps {number of occurences, insertion number} => val
+    map<pair<int, long>, int> _stack;
+    
+public:
+    FreqStack() {}
+    
+    void push(int val) {
+        _stack[{_counts[val], _num_pushes}] = val;
+        _counts[val]++;
+        _num_pushes++;
+    }
+    
+    int pop() {
+        int val = _stack.rbegin()->second;
+        _stack.erase(prev(_stack.end()));
+        _counts[val]--;
+        return val;
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ */
