16ab830730f05a03f9065f407c832b863638e246
Lorian Richmond <58707942+b4shfire@users.noreply.github.com>
2022-05-11 09:28:00 +0100

    Create 1641.cc

---
+++ medium/1641.cc
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-sorted-vowel-strings/
+
+class Solution {
+public:
+    int countVowelStrings(int n) {
+
+        return ((n+1)*(n+2)*(n+3)*(n+4))/24;
+    }
+};
