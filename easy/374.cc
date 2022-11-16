fb881922a45bab1249a0af7562854f365cfe89e6
Lorian Richmond <58707942+b4shfire@users.noreply.github.com>
2022-11-16 22:59:37 +0000

    Create 374.cc

---
+++ easy/374.cc
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/guess-number-higher-or-lower/
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        
+        int l=1, r=n;
+        
+        while (l<r) {
+            int m = l + (r-l)/2;
+            
+            int result = guess(m);
+            
+            if (result < 0)
+                r = m - 1;
+            else if (result > 0)
+                l = m + 1;
+            else
+                return m;
+        }
+        
+        return l;
+    }
+};
